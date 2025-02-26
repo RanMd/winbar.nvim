07806d9e7e5b0b5201647b63fdcb7cd9e98b3081
RanMd <randommid1873@gmail.com>
2025-02-25 22:55:19 -0500

    Remove schedule (unique option)

--- lua/winbar/init.lua
+++ lua/winbar/init.lua
@@ -17,6 +17,7 @@ function M.get_winbar(opts)
   local icon, hl_icon = "", ""
   local hl_bfn = "WinBar"
   local should_dim = not opts.active and config.options.dim_inactive.enabled
+  vim.print("inactive:" .. tostring(should_dim) .. vim.api.nvim_get_current_win())
 
   if config.options.diagnostics then
     diagnostics = utils.get_diagnostics()
@@ -27,7 +28,24 @@ function M.get_winbar(opts)
   end
 
   -- Build section a (icon)
-  local sectionA = table.concat({ " %#", hl_icon, "#", icon, " " })
+  local sectionA = " %#" .. hl_icon .. "#" .. icon .. " "
+
+  -- Build section c (path)
+  local sectionC = ""
+
+  -- Don't highlight name if the window is not active
+  if should_dim then
+    hl_bfn = config.options.dim_inactive.highlight
+  else
+    sectionC = "%#WinBarDir#" .. get_path()
+  end
+
+  -- Build section d (buffer modified)
+  local sectionD = ""
+
+  if vim.api.nvim_get_option_value("mod", {}) and config.options.buf_modified_symbol then
+    sectionD = M.mod_icon
+  end
 
   if diagnostics.level == "error" then
     hl_bfn = "DiagnosticError"
@@ -39,25 +57,10 @@ function M.get_winbar(opts)
     hl_bfn = "DiagnosticHint"
   end
 
-  -- Build section b (buffer name )
-  local sectionB = table.concat({ " %#", hl_bfn, "%#", "%t " })
+  -- Build section b (buffer name)
+  local sectionB = "%#" .. hl_bfn .. "# %t "
 
-  -- Don't highlight name if the window is not active
-  if should_dim then
-    hl_bfn = config.options.dim_inactive.highlight
-  end
-
-  -- Build section c (path)
-  local sectionC = "%#WinBarDir#" .. get_path()
-
-  -- Build section d (buffer modified)
-  local sectionD = ""
-
-  if vim.api.nvim_get_option_value("mod", {}) and config.options.buf_modified_symbol then
-    sectionD = M.mod_icon
-  end
-
-  local winbar = table.concat({ sectionA, sectionB, sectionC, sectionD, "%*" })
+  local winbar = sectionA .. sectionB .. sectionC .. sectionD
 
   return winbar
 end
@@ -71,34 +74,72 @@ function M.register()
   vim.api.nvim_create_autocmd(events, {
     group = augroup("winbar"),
     callback = function(args)
-      vim.schedule(function()
-        local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
-        for _, pattern in ipairs(config.options.filetype_exclude) do
-          if vim.fn.match(filetype, pattern) ~= -1 then
-            local ok, winbar_set_by_plugin = pcall(vim.api.nvim_buf_get_var, 0, "winbar_set_by_winbar_nvim")
-            if ok and winbar_set_by_plugin then
-              vim.opt_local.winbar = nil
-              vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", false)
-            end
-            return
+      local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
+      for _, pattern in ipairs(config.options.filetype_exclude) do
+        if vim.fn.match(filetype, pattern) ~= -1 then
+          local ok, winbar_set_by_plugin = pcall(vim.api.nvim_buf_get_var, 0, "winbar_set_by_winbar_nvim")
+          if ok and winbar_set_by_plugin then
+            -- print("winbar eliminado")
+            vim.opt_local.winbar = nil
+            vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", false)
           end
+          return
         end
+      end
 
-        local win_number = vim.api.nvim_get_current_win()
-        local win_config = vim.api.nvim_win_get_config(win_number)
+      local win_number = vim.api.nvim_get_current_win()
+      local win_config = vim.api.nvim_win_get_config(win_number)
 
-        if win_config.relative == "" then
-          local bar = " " .. "%*" .. M.get_winbar({ active = args.event ~= "WinLeave" }) .. "%*"
-          vim.api.nvim_set_option_value("winbar", bar, { scope = "local", win = win_number })
-          vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", true)
-        else
-          vim.opt_local.winbar = nil
-          vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", false)
-        end
-      end)
+      if win_config.relative == "" then
+        local bar = M.get_winbar({ active = args.event ~= "WinLeave" }) .. "%*"
+        -- print("Nuevo winbar:", bar)
+        vim.opt_local.winbar = bar
+        vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", true)
+      else
+        vim.opt_local.winbar = nil
+        vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", false)
+      end
     end,
   })
 end
+
+-- function M.register()
+--   local events = { "VimEnter", "BufEnter", "BufModifiedSet", "WinEnter", "WinLeave" }
+--   if config.options.diagnostics then
+--     table.insert(events, "DiagnosticChanged")
+--   end
+--
+--   vim.api.nvim_create_autocmd(events, {
+--     group = augroup("winbar"),
+--     callback = function(args)
+--       vim.schedule(function()
+--         local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
+--         for _, pattern in ipairs(config.options.filetype_exclude) do
+--           if vim.fn.match(filetype, pattern) ~= -1 then
+--             local ok, winbar_set_by_plugin = pcall(vim.api.nvim_buf_get_var, 0, "winbar_set_by_winbar_nvim")
+--             if ok and winbar_set_by_plugin then
+--               vim.opt_local.winbar = nil
+--               vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", false)
+--             end
+--             return
+--           end
+--         end
+--
+--         local win_number = vim.api.nvim_get_current_win()
+--         local win_config = vim.api.nvim_win_get_config(win_number)
+--
+--         if win_config.relative == "" then
+--           local bar = " " .. "%*" .. M.get_winbar({ active = args.event ~= "WinLeave" }) .. "%*"
+--           vim.api.nvim_set_option_value("winbar", bar, { scope = "local", win = win_number })
+--           vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", true)
+--         else
+--           vim.opt_local.winbar = nil
+--           vim.api.nvim_buf_set_var(0, "winbar_set_by_winbar_nvim", false)
+--         end
+--       end)
+--     end,
+--   })
+-- end
 
 function M.setup(options)
   config.setup(options)
